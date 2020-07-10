668eeb178024b7d5dabe7fdfdb9a337faf1cf242
leonardoramosc <leorrc96@gmail.com>
2020-07-10 09:31:54 -0400

    Updates

---
+++ precache-manifest.3ac39db1c7b729ffb7280fb15652f252.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "596dfd46ff4cdbc7e805d46027cb675f",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "7a1ed1f2b71ef7cc36be",
+    "url": "/monster-rolodex/static/css/main.58dbbc7f.chunk.css"
+  },
+  {
+    "revision": "9da1dca6a8d350eb302a",
+    "url": "/monster-rolodex/static/js/2.d6da0c6c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.d6da0c6c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a1ed1f2b71ef7cc36be",
+    "url": "/monster-rolodex/static/js/main.54315c32.chunk.js"
+  },
+  {
+    "revision": "b594faea7a9e2c9db0ba",
+    "url": "/monster-rolodex/static/js/runtime-main.649fb481.js"
+  }
+]);
